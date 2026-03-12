b1d941d0703cf3e8155608667924d79b48a0bb5c
Yaghcha Wissal <wissal.yaghcha@inetum.com>
2026-03-12 12:27:26 +0000

    Initial commit - projet Angular standaloneDTS-FRONT-V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { PanierComponent } from './panier/panier.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+ imports: [
+    CommonModule,
+    RouterOutlet,
+    PanierComponent
+  ]
+
 })
 export class AppComponent {
   title = 'DTS-FRONT-V1';
---
+++ src/app/panier/panier.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-panier',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './panier.component.html',
+  styleUrls: ['./panier.component.css']
+})
+export class PanierComponent {
+
+}
